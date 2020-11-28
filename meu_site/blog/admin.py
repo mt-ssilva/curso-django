186e6468395246b59f6f9ad5a90587acd2ea18ec
Matheus Santos <matheus.ssilva@upe.br>
2020-11-28 11:28:05 -0300

    criacao da classe PostAdmin

--- meu_site/blog/admin.py
+++ meu_site/blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('titulo','autor','publicado','status',)
+    list_filter = ('status','publicado','criado','autor',)
+    date_hierarchy = 'publicado'
+    search_fields = ('titulo','conteudo')
+    prepopulated_fields = {'slug':('titulo',)}
+
 
 # Register your models here.
